0d1a7904eab846b58a67b68f56b5c4d1291b0d2e
Andrew Hill <andrew@portworx.com>
2019-08-20 13:45:13 +0100

    hide output for 2 commands

--- aws-create-vpc.sh
+++ aws-create-vpc.sh
@@ -7,8 +7,8 @@ subnet=$(aws --output json ec2 create-subnet --vpc-id $vpc --cidr-block 192.168.
 gw=$(aws --output json ec2 create-internet-gateway | json InternetGateway.InternetGatewayId)
 aws ec2 attach-internet-gateway --vpc-id $vpc --internet-gateway-id $gw
 routetable=$(aws --output json ec2 create-route-table --vpc-id $vpc | json RouteTable.RouteTableId)
-aws ec2 create-route --route-table-id $routetable --destination-cidr-block 0.0.0.0/0 --gateway-id $gw
-aws ec2 associate-route-table  --subnet-id $subnet --route-table-id $routetable
+aws ec2 create-route --route-table-id $routetable --destination-cidr-block 0.0.0.0/0 --gateway-id $gw >/dev/null
+aws ec2 associate-route-table  --subnet-id $subnet --route-table-id $routetable >/dev/null
 sg=$(aws --output json ec2 create-security-group --group-name SSHAccess --description "Security group for SSH access" --vpc-id $vpc | json GroupId)
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 443 --cidr 0.0.0.0/0
