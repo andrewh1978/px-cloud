2cd37b8a40d23c85e09224a4767fd11fd14a96fe
Andrew Hill <andrew@portworx.com>
2019-10-09 17:29:29 +0100

    change subnet to allow >9 clusters; open internal traffic

--- gcp-create-project.sh
+++ gcp-create-project.sh
@@ -1,14 +1,24 @@
-. gcp-env.sh
+# Set the GCP region and project name
+GCP_REGION=europe-west1
+GCP_PROJECT=pxtest-1231234
 
+# Do not change below this line
 gcloud projects create $GCP_PROJECT
 account=$(gcloud alpha billing accounts list | tail -1 | cut -f 1 -d " ")
 gcloud alpha billing projects link $GCP_PROJECT --billing-account $account
 gcloud services enable compute.googleapis.com --project $GCP_PROJECT
 gcloud compute networks create px-net --project $GCP_PROJECT
-gcloud compute networks subnets create --range 192.168.99.0/24 --network px-net px-subnet --region $GCP_REGION --project $GCP_PROJECT
+gcloud compute networks subnets create --range 192.168.0.0/16 --network px-net px-subnet --region $GCP_REGION --project $GCP_PROJECT
+gcloud compute firewall-rules create allow-tcp --allow=tcp --source-ranges=192.168.0.0/16 --network px-net --project $GCP_PROJECT
+gcloud compute firewall-rules create allow-udp --allow=udp --source-ranges=192.168.0.0/16 --network px-net --project $GCP_PROJECT
+gcloud compute firewall-rules create allow-icmp --allow=icmp --source-ranges=192.168.0.0/16 --network px-net --project $GCP_PROJECT
 gcloud compute firewall-rules create allow-ssh --allow=tcp:22 --network px-net --project $GCP_PROJECT
 gcloud compute firewall-rules create allow-https --allow=tcp:443 --network px-net --project $GCP_PROJECT
 gcloud compute firewall-rules create allow-k8s --allow=tcp:6443 --network px-net --project $GCP_PROJECT
-gcloud compute firewall-rules create allow-px-tcp --allow=tcp:9001-9022 --network px-net --project $GCP_PROJECT
-gcloud compute firewall-rules create allow-px-udp --allow=udp:9002 --network px-net --project $GCP_PROJECT
 gcloud compute project-info add-metadata --metadata "ssh-keys=$USER:$(cat $HOME/.ssh/id_rsa.pub)" --project $GCP_PROJECT
+
+cat <<EOF >gcp-env.sh
+GCP_PROJECT=$GCP_PROJECT
+GCP_REGION=$GCP_REGION
+export GCP_PROJECT GCP_REGION
+EOF
