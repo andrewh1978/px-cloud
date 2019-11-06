7da0d3e35a97365e28c5d3ad264bfe9f06a5f5f7
Andrew Hill <andrew@portworx.com>
2019-11-06 13:32:08 +0000

    simplify and rename security group

--- aws-create-vpc.sh
+++ aws-create-vpc.sh
@@ -9,13 +9,11 @@ aws ec2 attach-internet-gateway --vpc-id $vpc --internet-gateway-id $gw
 routetable=$(aws --output json ec2 create-route-table --vpc-id $vpc | json RouteTable.RouteTableId)
 aws ec2 create-route --route-table-id $routetable --destination-cidr-block 0.0.0.0/0 --gateway-id $gw >/dev/null
 aws ec2 associate-route-table  --subnet-id $subnet --route-table-id $routetable >/dev/null
-sg=$(aws --output json ec2 create-security-group --group-name SSHAccess --description "Security group for SSH access" --vpc-id $vpc | json GroupId)
+sg=$(aws --output json ec2 create-security-group --group-name px-cloud --description "Security group for px-cloud" --vpc-id $vpc | json GroupId)
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 443 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 8443 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 32678 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 30900 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 30950 --cidr 0.0.0.0/0
++aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 30000-32767 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol all --cidr 192.168.0.0/16
 
 ami=$(aws --output json ec2 describe-images --owners 679593333241 --filters Name=name,Values='CentOS Linux 7 x86_64 HVM EBS*' Name=architecture,Values=x86_64 Name=root-device-type,Values=ebs --query 'sort_by(Images, &Name)[-1].ImageId' --output text)
