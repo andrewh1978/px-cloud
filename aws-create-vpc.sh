f0e6bf4f1057a69263ac167f8325ec6c51de1a08
Andrew Hill <andrew@portworx.com>
2019-11-26 16:53:16 +0000

    change port

--- aws-create-vpc.sh
+++ aws-create-vpc.sh
@@ -12,7 +12,7 @@ aws ec2 associate-route-table  --subnet-id $subnet --route-table-id $routetable 
 sg=$(aws --output json ec2 create-security-group --group-name px-cloud --description "Security group for px-cloud" --vpc-id $vpc | json GroupId)
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 443 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 8443 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 8080 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 30000-32767 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sg --protocol all --cidr 192.168.0.0/16
 
