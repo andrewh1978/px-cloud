94f57f36ded2ce6a657e86237e73f2020f248022
Andrew Hill <andrew@portworx.com>
2020-01-24 13:34:21 +0000

    speedup for gcp project creation

--- gcp-create-project.sh
+++ gcp-create-project.sh
@@ -10,11 +10,12 @@ gcloud alpha billing projects link $GCP_PROJECT --billing-account $account
 gcloud services enable compute.googleapis.com --project $GCP_PROJECT
 gcloud compute networks create px-net --project $GCP_PROJECT
 gcloud compute networks subnets create --range 192.168.0.0/16 --network px-net px-subnet --region $GCP_REGION --project $GCP_PROJECT
-gcloud compute firewall-rules create allow-internal --allow=tcp,udp,icmp --source-ranges=192.168.0.0/16 --network px-net --project $GCP_PROJECT
-gcloud compute firewall-rules create allow-external --allow=tcp:22,tcp:443,tcp:6443 --network px-net --project $GCP_PROJECT
-gcloud compute project-info add-metadata --metadata "ssh-keys=$USER:$(cat $HOME/.ssh/id_rsa.pub)" --project $GCP_PROJECT
+gcloud compute firewall-rules create allow-internal --allow=tcp,udp,icmp --source-ranges=192.168.0.0/16 --network px-net --project $GCP_PROJECT &
+gcloud compute firewall-rules create allow-external --allow=tcp:22,tcp:443,tcp:6443 --network px-net --project $GCP_PROJECT &
+gcloud compute project-info add-metadata --metadata "ssh-keys=$USER:$(cat $HOME/.ssh/id_rsa.pub)" --project $GCP_PROJECT &
 service_account=$(gcloud iam service-accounts list --project $GCP_PROJECT --format 'flattened(email)' | tail -1 | cut -f 2 -d " ")
 GCP_key=$(gcloud iam service-accounts keys create /dev/stdout --iam-account $service_account | base64)
+wait
 
 cat <<EOF >gcp-env.sh
 GCP_PROJECT=$GCP_PROJECT
