eedc17123d6b031130d4e48ffba71b90893f5c68
Andrew Hill <andrew@portworx.com>
2020-01-24 14:17:56 +0000

    speedup for aws vpc creation

--- aws-create-vpc.sh
+++ aws-create-vpc.sh
@@ -13,11 +13,12 @@ AWS_routetable=$(aws --region=$AWS_region --output text ec2 create-route-table -
 aws --region=$AWS_region ec2 create-route --route-table-id $AWS_routetable --destination-cidr-block 0.0.0.0/0 --gateway-id $AWS_gw >/dev/null
 aws --region=$AWS_region ec2 associate-route-table  --subnet-id $AWS_subnet --route-table-id $AWS_routetable >/dev/null
 AWS_sg=$(aws --region=$AWS_region --output text ec2 create-security-group --group-name px-cloud --description "Security group for px-cloud" --vpc-id $AWS_vpc --query GroupId)
-aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol tcp --port 22 --cidr 0.0.0.0/0
-aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol tcp --port 443 --cidr 0.0.0.0/0
-aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol tcp --port 8080 --cidr 0.0.0.0/0
-aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol tcp --port 30000-32767 --cidr 0.0.0.0/0
-aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol all --cidr 192.168.0.0/16
+aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol tcp --port 22 --cidr 0.0.0.0/0 &
+aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol tcp --port 443 --cidr 0.0.0.0/0 &
+aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol tcp --port 8080 --cidr 0.0.0.0/0 &
+aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol tcp --port 30000-32767 --cidr 0.0.0.0/0 &
+aws --region=$AWS_region ec2 authorize-security-group-ingress --group-id $AWS_sg --protocol all --cidr 192.168.0.0/16 &
+wait
 
 AWS_ami=$(aws --region=$AWS_region --output text ec2 describe-images --owners 679593333241 --filters Name=name,Values='CentOS Linux 7 x86_64 HVM EBS*' Name=architecture,Values=x86_64 Name=root-device-type,Values=ebs --query 'sort_by(Images, &Name)[-1].ImageId')
 
