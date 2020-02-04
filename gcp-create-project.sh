9891d4b0e616ad4d4bd23d4315e286ac75f254cd
Andrew Hill <andrew@portworx.com>
2020-02-04 14:44:30 +0000

    change gcp username

--- gcp-create-project.sh
+++ gcp-create-project.sh
@@ -13,7 +13,7 @@ gcloud compute networks create px-net --project $GCP_PROJECT
 gcloud compute networks subnets create --range 192.168.0.0/16 --network px-net px-subnet --region $GCP_REGION --project $GCP_PROJECT
 gcloud compute firewall-rules create allow-internal --allow=tcp,udp,icmp --source-ranges=192.168.0.0/16 --network px-net --project $GCP_PROJECT &
 gcloud compute firewall-rules create allow-external --allow=tcp:22,tcp:443,tcp:6443 --network px-net --project $GCP_PROJECT &
-gcloud compute project-info add-metadata --metadata "ssh-keys=$USER:$(cat id_rsa.gcp.pub)" --project $GCP_PROJECT &
+gcloud compute project-info add-metadata --metadata "ssh-keys=centos:$(cat id_rsa.gcp.pub)" --project $GCP_PROJECT &
 service_account=$(gcloud iam service-accounts list --project $GCP_PROJECT --format 'flattened(email)' | tail -1 | cut -f 2 -d " ")
 GCP_key="$(gcloud iam service-accounts keys create /dev/stdout --iam-account $service_account)"
 wait
